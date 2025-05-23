964cfe02710e602233a79824ab5e96f2355e2e40
Templa <termeh.sh98@gmail.com>
2025-05-22 20:20:13 -0400

    updated experiment verifier

--- verify.js
+++ verify.js
@@ -71,6 +71,12 @@ function validateConfig(config) {
 
     if ('run_experiment' in settings && !isBoolean(settings.run_experiment)) {
       errors.push('"settings.run_experiment" must be a boolean.');
+
+      // return and don't verify the configs if the experiment is not running
+      if(!settings.run_experiment){
+        console.log("Verification skipped for this experiemnt as 'run_experiment' is set to 'false'.")
+        return [];
+      }
     }
     if ('name' in settings && !isString(settings.name)) {
       errors.push('"settings.name" must be a string.');
@@ -108,21 +114,24 @@ function validateConfig(config) {
         }
       });
 
+      // check for quality control pipeline
       if (!isBoolean(pipeline.quality_control)) {
         errors.push('"settings.pipeline.quality_control" must be a boolean.');
       } else {
         pipeline_stages.qc = pipeline.quality_control;
       }
-      if ('dada' in pipeline && !isBoolean(pipeline.dada)) {
+
+      //check for dada2 pipeline
+      if (!isBoolean(pipeline.dada)) {
         errors.push('"settings.pipeline.dada" must be a boolean.');
       } else {
         pipeline_stages.dada = pipeline.dada;
       }
+
     }
   }
 
   // Validate "input_data" section
-
   if (!config.input_data) {
     errors.push('Missing "input_data" section.');
   } else {
@@ -197,7 +206,7 @@ function validateConfig(config) {
       const qc = config.quality_control;
 
       // Check for required keys in "quality_control"
-      const requiredQcKeys = ['quality_profile_plot', 'rarefaction_curve', 'multiqc'];
+      const requiredQcKeys = ['quality_profile_plot']; // dropped support for 'rarefaction_curve', 'multiqc' 
       requiredQcKeys.forEach((key) => {
         if (!(key in qc)) {
           errors.push(`Missing "quality_control.${key}" field.`);
@@ -207,216 +216,230 @@ function validateConfig(config) {
       if ('quality_profile_plot' in qc && !isBoolean(qc.quality_profile_plot)) {
         errors.push('"quality_control.quality_profile_plot" must be a boolean.');
       }
-      if ('rarefaction_curve' in qc && !isBoolean(qc.rarefaction_curve)) {
-        errors.push('"quality_control.rarefaction_curve" must be a boolean.');
-      }
 
-      if ('multiqc' in qc) {
-        const multiqc = qc.multiqc;
+      // TODO: fix multiqc on compute canada and 
+      // if ('rarefaction_curve' in qc && !isBoolean(qc.rarefaction_curve)) {
+      //   errors.push('"quality_control.rarefaction_curve" must be a boolean.');
+      // }
 
-        // Check for required keys in "multiqc"
-        const requiredMultiqcKeys = [
-          'separate_direction_reports',
-          'delete_intermediate_files',
-          'interactive_plots',
-          // 'configs' is optional
-        ];
-        requiredMultiqcKeys.forEach((key) => {
-          if (!(key in multiqc)) {
-            errors.push(`Missing "quality_control.multiqc.${key}" field.`);
+      // if ('multiqc' in qc) {
+      //   const multiqc = qc.multiqc;
+
+      //   // Check for required keys in "multiqc"
+      //   const requiredMultiqcKeys = [
+      //     'separate_direction_reports',
+      //     'delete_intermediate_files',
+      //     'interactive_plots',
+      //     // 'configs' is optional
+      //   ];
+      //   requiredMultiqcKeys.forEach((key) => {
+      //     if (!(key in multiqc)) {
+      //       errors.push(`Missing "quality_control.multiqc.${key}" field.`);
+      //     }
+      //   });
+
+      //   if (
+      //     'separate_direction_reports' in multiqc &&
+      //     !isBoolean(multiqc.separate_direction_reports)
+      //   ) {
+      //     errors.push('"quality_control.multiqc.separate_direction_reports" must be a boolean.');
+      //   }
+      //   if ('delete_intermediate_files' in multiqc && !isBoolean(multiqc.delete_intermediate_files)) {
+      //     errors.push('"quality_control.multiqc.delete_intermediate_files" must be a boolean.');
+      //   }
+      //   if ('interactive_plots' in multiqc && !isBoolean(multiqc.interactive_plots)) {
+      //     errors.push('"quality_control.multiqc.interactive_plots" must be a boolean.');
+      //   }
+      //   if ('configs' in multiqc && multiqc.configs !== undefined && !isString(multiqc.configs)) {
+      //     errors.push('"quality_control.multiqc.configs" must be a string if provided.');
+      //   }
+      // }
+    }
+  }
+
+  // validate dada2 pipeline
+  if (pipeline_stages.dada){
+    if(!config.dada){
+      errors.push('Missing "dada" pipeline section.');
+    } else{
+      // Validate "filter_and_trim" section
+      if (!config.dada.filter_and_trim) {
+        errors.push('Missing "filter_and_trim" section.');
+      } else {
+        const ft = config.dada.filter_and_trim;
+
+        // Check for required keys in "filter_and_trim"
+        const requiredFtKeys = ['remove_phix_genome', 'min_read_length', 'truncate', 'trim_left'];
+        requiredFtKeys.forEach((key) => {
+          if (!(key in ft)) {
+            errors.push(`Missing "filter_and_trim.${key}" field.`);
           }
         });
 
-        if (
-          'separate_direction_reports' in multiqc &&
-          !isBoolean(multiqc.separate_direction_reports)
-        ) {
-          errors.push('"quality_control.multiqc.separate_direction_reports" must be a boolean.');
+        if ('remove_phix_genome' in ft && !isBoolean(ft.remove_phix_genome)) {
+          errors.push('"filter_and_trim.remove_phix_genome" must be a boolean.');
         }
-        if ('delete_intermediate_files' in multiqc && !isBoolean(multiqc.delete_intermediate_files)) {
-          errors.push('"quality_control.multiqc.delete_intermediate_files" must be a boolean.');
+        if ('min_read_length' in ft && !isInt(ft.min_read_length)) {
+          errors.push('"filter_and_trim.min_read_length" must be an integer.');
         }
-        if ('interactive_plots' in multiqc && !isBoolean(multiqc.interactive_plots)) {
-          errors.push('"quality_control.multiqc.interactive_plots" must be a boolean.');
+
+        if (!('truncate' in ft)) {
+          errors.push('Missing "filter_and_trim.truncate" section.');
+        } else {
+          const truncate = ft.truncate;
+
+          // Check for required keys in "truncate"
+          const requiredTruncateKeys = ['forward', 'reverse'];
+          requiredTruncateKeys.forEach((key) => {
+            if (!(key in truncate)) {
+              errors.push(`Missing "filter_and_trim.truncate.${key}" field.`);
+            }
+          });
+
+          if ('forward' in truncate && !isInt(truncate.forward)) {
+            errors.push('"filter_and_trim.truncate.forward" must be an integer.');
+          }
+          if ('reverse' in truncate && !isInt(truncate.reverse)) {
+            errors.push('"filter_and_trim.truncate.reverse" must be an integer.');
+          }
         }
-        if ('configs' in multiqc && multiqc.configs !== undefined && !isString(multiqc.configs)) {
-          errors.push('"quality_control.multiqc.configs" must be a string if provided.');
+
+        if (!('trim_left' in ft)) {
+          errors.push('Missing "filter_and_trim.trim_left" section.');
+        } else {
+          const trimLeft = ft.trim_left;
+
+          // Check for required keys in "trim_left"
+          const requiredTrimLeftKeys = ['forward', 'reverse'];
+          requiredTrimLeftKeys.forEach((key) => {
+            if (!(key in trimLeft)) {
+              errors.push(`Missing "filter_and_trim.trim_left.${key}" field.`);
+            }
+          });
+
+          if ('forward' in trimLeft && !isInt(trimLeft.forward)) {
+            errors.push('"filter_and_trim.trim_left.forward" must be an integer.');
+          }
+          if ('reverse' in trimLeft && !isInt(trimLeft.reverse)) {
+            errors.push('"filter_and_trim.trim_left.reverse" must be an integer.');
+          }
         }
+      }
+
+      // Validate "asv_inference" section
+      if (!config.dada.asv_inference) {
+        errors.push('Missing "asv_inference" section.');
+      } else {
+        const asv = config.dada.asv_inference;
+
+        // Check for required keys in "asv_inference"
+        const requiredAsvKeys = ['error_model', 'dada_pool_samples', 'generate_fasta_file'];
+        requiredAsvKeys.forEach((key) => {
+          if (!(key in asv)) {
+            errors.push(`Missing "asv_inference.${key}" field.`);
+          }
+        });
+
+        if (!('error_model' in asv)) {
+          errors.push('Missing "asv_inference.error_model" section.');
+        } else {
+          const errorModel = asv.error_model;
+
+          // Check for required keys in "error_model"
+          const requiredErrorModelKeys = ['randomize', 'iterations'];
+          requiredErrorModelKeys.forEach((key) => {
+            if (!(key in errorModel)) {
+              errors.push(`Missing "asv_inference.error_model.${key}" field.`);
+            }
+          });
+
+          if ('randomize' in errorModel && !isBoolean(errorModel.randomize)) {
+            errors.push('"asv_inference.error_model.randomize" must be a boolean.');
+          }
+          if ('iterations' in errorModel) {
+            if (!isInt(errorModel.iterations)) {
+              errors.push('"asv_inference.error_model.iterations" must be an integer.');
+            } else if (!checkRange(errorModel.iterations, 1, 30)) {
+              errors.push('"asv_inference.error_model.iterations" must be between 1 and 30 inclusive.');
+            }
+          }
+        }
+
+        if ('dada_pool_samples' in asv && !isBoolean(asv.dada_pool_samples)) {
+          errors.push('"asv_inference.dada_pool_samples" must be a boolean.');
+        }
+
+        if ('generate_fasta_file' in asv && !isBoolean(asv.generate_fasta_file)) {
+          errors.push('"asv_inference.generate_fasta_file" must be a boolean.');
+        }
+      }
+
+      // Validate "taxonomy_assignment" section
+      if (!('taxonomy_assignment' in config.dada)) {
+        errors.push('Missing "taxonomy_assignment" section.');
+      } else if (!Array.isArray(config.dada.taxonomy_assignment)) {
+        errors.push('"taxonomy_assignment" must be an array.');
+      } else {
+        config.dada.taxonomy_assignment.forEach((ta, index) => {
+          // Check for required keys in each taxonomy assignment
+          const requiredTaKeys = [
+            'active',
+            'reference_name',
+            'train_set_path',
+            'assign_species',
+            'allow_multiple_species',
+            'species_train_set_path',
+            'reverse_match_taxa',
+          ];
+          requiredTaKeys.forEach((key) => {
+            if (!(key in ta)) {
+              errors.push(`Missing "taxonomy_assignment[${index}].${key}" field.`);
+            }
+          });
+
+          if ('active' in ta && !isBoolean(ta.active)) {
+            errors.push(`"taxonomy_assignment[${index}].active" must be a boolean.`);
+          }
+          if ('reference_name' in ta && !isString(ta.reference_name)) {
+            errors.push(`"taxonomy_assignment[${index}].reference_name" must be a string.`);
+          }
+          if ('train_set_path' in ta) {
+            if (!isString(ta.train_set_path) || !isUnixPath(ta.train_set_path)) {
+              errors.push(
+                `"taxonomy_assignment[${index}].train_set_path" must be a valid Unix file path.`
+              );
+            } else if (!pathExists(ta.train_set_path)) {
+              errors.push(
+                `"taxonomy_assignment[${index}].train_set_path" path does not exist: ${ta.train_set_path}`
+              );
+            }
+          }
+          if ('assign_species' in ta && !isBoolean(ta.assign_species)) {
+            errors.push(`"taxonomy_assignment[${index}].assign_species" must be a boolean.`);
+          }
+          if ('allow_multiple_species' in ta && !isBoolean(ta.allow_multiple_species)) {
+            errors.push(`"taxonomy_assignment[${index}].allow_multiple_species" must be a boolean.`);
+          }
+          if ('species_train_set_path' in ta) {
+            if (!isString(ta.species_train_set_path) || !isUnixPath(ta.species_train_set_path)) {
+              errors.push(
+                `"taxonomy_assignment[${index}].species_train_set_path" must be a valid Unix file path.`
+              );
+            } else if (!pathExists(ta.species_train_set_path)) {
+              errors.push(
+                `"taxonomy_assignment[${index}].species_train_set_path" path does not exist: ${ta.species_train_set_path}`
+              );
+            }
+          }
+          if ('reverse_match_taxa' in ta && !isBoolean(ta.reverse_match_taxa)) {
+            errors.push(`"taxonomy_assignment[${index}].reverse_match_taxa" must be a boolean.`);
+          }
+        });
       }
     }
   }
 
-
-  if (pipeline_stages.dada){// Validate "filter_and_trim" section
-  if (!config.filter_and_trim) {
-    errors.push('Missing "filter_and_trim" section.');
-  } else {
-    const ft = config.filter_and_trim;
-
-    // Check for required keys in "filter_and_trim"
-    const requiredFtKeys = ['remove_phix_genome', 'min_read_length', 'truncate', 'trim_left'];
-    requiredFtKeys.forEach((key) => {
-      if (!(key in ft)) {
-        errors.push(`Missing "filter_and_trim.${key}" field.`);
-      }
-    });
-
-    if ('remove_phix_genome' in ft && !isBoolean(ft.remove_phix_genome)) {
-      errors.push('"filter_and_trim.remove_phix_genome" must be a boolean.');
-    }
-    if ('min_read_length' in ft && !isInt(ft.min_read_length)) {
-      errors.push('"filter_and_trim.min_read_length" must be an integer.');
-    }
-
-    if (!('truncate' in ft)) {
-      errors.push('Missing "filter_and_trim.truncate" section.');
-    } else {
-      const truncate = ft.truncate;
-
-      // Check for required keys in "truncate"
-      const requiredTruncateKeys = ['forward', 'reverse'];
-      requiredTruncateKeys.forEach((key) => {
-        if (!(key in truncate)) {
-          errors.push(`Missing "filter_and_trim.truncate.${key}" field.`);
-        }
-      });
-
-      if ('forward' in truncate && !isInt(truncate.forward)) {
-        errors.push('"filter_and_trim.truncate.forward" must be an integer.');
-      }
-      if ('reverse' in truncate && !isInt(truncate.reverse)) {
-        errors.push('"filter_and_trim.truncate.reverse" must be an integer.');
-      }
-    }
-
-    if (!('trim_left' in ft)) {
-      errors.push('Missing "filter_and_trim.trim_left" section.');
-    } else {
-      const trimLeft = ft.trim_left;
-
-      // Check for required keys in "trim_left"
-      const requiredTrimLeftKeys = ['forward', 'reverse'];
-      requiredTrimLeftKeys.forEach((key) => {
-        if (!(key in trimLeft)) {
-          errors.push(`Missing "filter_and_trim.trim_left.${key}" field.`);
-        }
-      });
-
-      if ('forward' in trimLeft && !isInt(trimLeft.forward)) {
-        errors.push('"filter_and_trim.trim_left.forward" must be an integer.');
-      }
-      if ('reverse' in trimLeft && !isInt(trimLeft.reverse)) {
-        errors.push('"filter_and_trim.trim_left.reverse" must be an integer.');
-      }
-    }
-  }
-
-  // Validate "asv_inference" section
-  if (!config.asv_inference) {
-    errors.push('Missing "asv_inference" section.');
-  } else {
-    const asv = config.asv_inference;
-
-    // Check for required keys in "asv_inference"
-    const requiredAsvKeys = ['error_model', 'dada_pool_samples'];
-    requiredAsvKeys.forEach((key) => {
-      if (!(key in asv)) {
-        errors.push(`Missing "asv_inference.${key}" field.`);
-      }
-    });
-
-    if (!('error_model' in asv)) {
-      errors.push('Missing "asv_inference.error_model" section.');
-    } else {
-      const errorModel = asv.error_model;
-
-      // Check for required keys in "error_model"
-      const requiredErrorModelKeys = ['randomize', 'iterations'];
-      requiredErrorModelKeys.forEach((key) => {
-        if (!(key in errorModel)) {
-          errors.push(`Missing "asv_inference.error_model.${key}" field.`);
-        }
-      });
-
-      if ('randomize' in errorModel && !isBoolean(errorModel.randomize)) {
-        errors.push('"asv_inference.error_model.randomize" must be a boolean.');
-      }
-      if ('iterations' in errorModel) {
-        if (!isInt(errorModel.iterations)) {
-          errors.push('"asv_inference.error_model.iterations" must be an integer.');
-        } else if (!checkRange(errorModel.iterations, 1, 30)) {
-          errors.push('"asv_inference.error_model.iterations" must be between 1 and 30 inclusive.');
-        }
-      }
-    }
-
-    if ('dada_pool_samples' in asv && !isBoolean(asv.dada_pool_samples)) {
-      errors.push('"asv_inference.dada_pool_samples" must be a boolean.');
-    }
-  }
-
-  // Validate "taxonomy_assignment" section
-  if (!('taxonomy_assignment' in config)) {
-    errors.push('Missing "taxonomy_assignment" section.');
-  } else if (!Array.isArray(config.taxonomy_assignment)) {
-    errors.push('"taxonomy_assignment" must be an array.');
-  } else {
-    config.taxonomy_assignment.forEach((ta, index) => {
-      // Check for required keys in each taxonomy assignment
-      const requiredTaKeys = [
-        'active',
-        'reference_name',
-        'train_set_path',
-        'assign_species',
-        'allow_multiple_species',
-        'species_train_set_path',
-        'reverse_match_taxa',
-      ];
-      requiredTaKeys.forEach((key) => {
-        if (!(key in ta)) {
-          errors.push(`Missing "taxonomy_assignment[${index}].${key}" field.`);
-        }
-      });
-
-      if ('active' in ta && !isBoolean(ta.active)) {
-        errors.push(`"taxonomy_assignment[${index}].active" must be a boolean.`);
-      }
-      if ('reference_name' in ta && !isString(ta.reference_name)) {
-        errors.push(`"taxonomy_assignment[${index}].reference_name" must be a string.`);
-      }
-      if ('train_set_path' in ta) {
-        if (!isString(ta.train_set_path) || !isUnixPath(ta.train_set_path)) {
-          errors.push(
-            `"taxonomy_assignment[${index}].train_set_path" must be a valid Unix file path.`
-          );
-        } else if (!pathExists(ta.train_set_path)) {
-          errors.push(
-            `"taxonomy_assignment[${index}].train_set_path" path does not exist: ${ta.train_set_path}`
-          );
-        }
-      }
-      if ('assign_species' in ta && !isBoolean(ta.assign_species)) {
-        errors.push(`"taxonomy_assignment[${index}].assign_species" must be a boolean.`);
-      }
-      if ('allow_multiple_species' in ta && !isBoolean(ta.allow_multiple_species)) {
-        errors.push(`"taxonomy_assignment[${index}].allow_multiple_species" must be a boolean.`);
-      }
-      if ('species_train_set_path' in ta) {
-        if (!isString(ta.species_train_set_path) || !isUnixPath(ta.species_train_set_path)) {
-          errors.push(
-            `"taxonomy_assignment[${index}].species_train_set_path" must be a valid Unix file path.`
-          );
-        } else if (!pathExists(ta.species_train_set_path)) {
-          errors.push(
-            `"taxonomy_assignment[${index}].species_train_set_path" path does not exist: ${ta.species_train_set_path}`
-          );
-        }
-      }
-      if ('reverse_match_taxa' in ta && !isBoolean(ta.reverse_match_taxa)) {
-        errors.push(`"taxonomy_assignment[${index}].reverse_match_taxa" must be a boolean.`);
-      }
-    });
-  }}
+  
 
   return errors;
 }
@@ -462,6 +485,7 @@ function validateConfig(config) {
   console.log(`Found ${bubuJsonConfig.length} experiments. Running verifier...`);
 
   for (config of bubuJsonConfig) {
+    
     const errors = validateConfig(config);
     console.log(`Verifying experiment ${current_exp_num}/${num_exp}`);
 
